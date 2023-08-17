f91a330c26bda794df104fe9720e559c5f97ab0c
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-08-17 16:48:45 +0800

    Create Product_Price_at_a_Given_Date.sql

---
+++ SQL 50/Product_Price_at_a_Given_Date.sql
@@ -0,0 +1,40 @@
+SELECT product_id, new_price as price
+FROM Products
+WHERE (product_id, change_date) IN
+(
+  SELECT product_id, MAX(change_date)
+  FROM Products
+  WHERE DATEDIFF(change_date, '2019-08-16')<=0
+  GROUP BY product_id
+)
+UNION
+SELECT product_id, 10 as price
+FROM Products
+WHERE product_id NOT IN
+(
+  SELECT DISTINCT product_id
+  FROM Products
+  WHERE DATEDIFF(change_date, '2019-08-16')<=0
+  GROUP BY product_id
+);
+
+-- -OR using VIEW
+CREATE VIEW [Change] AS
+SELECT product_id, MAX(change_date)
+FROM Products
+WHERE change_date<='2019-08-16'
+GROUP BY product_id;
+
+SELECT product_id, new_price as price
+FROM Products
+WHERE (product_id, change_date) IN [Change]
+UNION
+SELECT product_id, 10 as price
+FROM Products
+WHERE product_id NOT IN
+(
+  SELECT DISTINCT product_id
+  FROM [Change]
+);
+
+DROP VIEW [Change];
