588788de2d17fdc0e3d1167470356ab3ebb403a8
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-08-18 17:19:34 +0800

    Create List_the_Products_Ordered_in_a_Period.sql

---
+++ SQL 50/List_the_Products_Ordered_in_a_Period.sql
@@ -0,0 +1,6 @@
+SELECT product_name, SUM(unit) AS unit
+FROM Products
+LEFT JOIN Orders ON Products.product_id=Orders.product_id
+WHERE order_date BETWEEN "2020-02-01" AND "2020-02-29"
+GROUP BY Products.product_id
+HAVING SUM(unit)>=100;
