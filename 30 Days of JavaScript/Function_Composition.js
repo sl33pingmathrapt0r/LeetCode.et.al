67678ffe50bf1bc58c77d9a36e7f2c18b29a289d
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-08-21 16:53:43 +0800

    Create Function_Composition.js

---
+++ 30 Days of JavaScript/Function_Composition.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        var ans=x;
+        for (var i=functions.length-1; i>=0; i--) {
+            ans= functions[i](ans);
+        }
+        return ans;
+
+        // OR using reduceRight, 
+        /* 
+            if (functions.length === 0) {
+                return function(x) { return x; };
+            }
+
+            return functions.reduceRight(function(prevFn, nextFn) {
+                return function(x) {
+                return nextFn(prevFn(x));
+                };
+            });
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
