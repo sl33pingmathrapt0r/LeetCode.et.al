4efd436e1b0d3b63a4c70726fdc1f89ae420428b
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-08-22 15:09:03 +0800

    Create Sleep.js

---
+++ 30 Days of JavaScript/Sleep.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return await (new Promise(resolve =>setTimeout(resolve, millis)));
+    // return not necessary
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
