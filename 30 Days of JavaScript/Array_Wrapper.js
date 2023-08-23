acfff7663ba7a0406178fc89ebdd7c4d14fa6b70
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-08-23 14:02:40 +0800

    Create Array_Wrapper.js

---
+++ 30 Days of JavaScript/Array_Wrapper.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.nums= nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((sum, num) => sum + num, 0);
+}
+
+ArrayWrapper.prototype.toString = function() {
+    // add code in strings using ` `, which looks for ${}
+    return `[${this.nums.join(',')}]`;
+}
+
+// Alternatively, 
+// class ArrayWrapper {
+//     constructor(nums) {this.nums=nums;}
+
+//     valueOf= () => this.nums.reduce((sum, num) => sum + num, 0);
+//     // ^ can also use [...this.nums]
+//     toString= () => `[${this.nums.join(',')}]`;
+// }
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
