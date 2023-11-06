0450fbcf23a4b09ca1a87634c13eb5623189bd00
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2023-11-06 11:53:08 +0800

    improved with mergesort

--- Linked List/Merge_k_Sorted_Lists.py
+++ Linked List/Merge_k_Sorted_Lists.py
@@ -6,6 +6,10 @@
 class Solution:
     # first attempt
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
         cache = [lst for lst in lists if lst]
 
         cur = head = ListNode()
@@ -17,4 +21,44 @@ class Solution:
             if not cache[small]:
                 cache.pop(small)
         
+        return head.next
+
+    # second attempt
+    def mergeKLists(self, lists):
+        """
+        use merge sort, where each list
+        is the smallest section.
+        """
+        if len(lists)==0:
+            return None
+
+        while len(lists) >1:
+            lists = [self.merge(lists[2*i:2*i+2]) for i in range(len(lists)//2+1 if len(lists)%2 else len(lists)//2)]
+
+        return lists[0]
+
+
+    def merge(self,lst12):
+        """
+        helper function
+        """
+        if len(lst12) == 0:
+            return None
+
+        if len(lst12) == 1:
+            return lst12[0]
+        
+        lst1,lst2 = lst12
+
+        head = cur = ListNode()
+        while lst1 and lst2:
+            if lst1.val<lst2.val:
+                cur.next = lst1
+                lst1, cur = lst1.next, cur.next
+            else:
+                cur.next = lst2
+                lst2, cur = lst2.next, cur.next
+        
+        cur.next = lst1 if lst1 else lst2
+
         return head.next
